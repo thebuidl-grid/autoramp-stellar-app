3fe916d36efaa278210ae7e11484363211972cf4
emperorsixpacks <andrewdvd10@gmail.com>
2025-12-31 17:29:12 +0100

    refactor: move router module to app module

--- backend/src/modules/admin/admin.module.ts
+++ backend/src/modules/admin/admin.module.ts
@@ -3,7 +3,6 @@ import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { ApiKeysModule } from '../api-keys/api-keys.module';
 import { TransactionsModule } from './transactions/transactions.module';
-import { RouterModule } from '@nestjs/core';
 
 /**
  * Admin Module
@@ -13,22 +12,7 @@ import { RouterModule } from '@nestjs/core';
  * - API key management
  */
 @Module({
-  imports: [
-    ApiKeysModule,
-    TransactionsModule,
-    RouterModule.register([
-      {
-        path: 'admin',
-        module: AdminModule,
-        children: [
-          {
-            path: 'transactions',
-            module: TransactionsModule,
-          },
-        ],
-      },
-    ]),
-  ],
+  imports: [ApiKeysModule, TransactionsModule],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [AdminService],
